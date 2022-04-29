49ebe7de3eac919d5ba2370efb0f1ecfaa79a3f0
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-04-29 14:20:18 +0600

    client side inisial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world this assignment 11 WOW</h1>
+      <h1>this is md shakil</h1>
     </div>
   );
 }
