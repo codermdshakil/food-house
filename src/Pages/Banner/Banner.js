e7f99cee6702e1b959b7880b9aeaed7f79d9df87
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-04-30 18:01:29 +0600

    some change that i make in design

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -19,22 +19,7 @@ const Banner = () => {
                 modules={[Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <div className="item-container">
-                        <div className="container">
-                            <div className='row'>
-                                <div className="col-lg-6 col-md-6 ">
-                                    <div className='product-banner-title '>
-                                        <p className='feaured-title'>Feature Product</p>
-                                        <h2>May <span className='highlight'>Protect</span> your brain as you age</h2>
-                                        <p>Contains beneficial plant compounds. Apples are rich in plant compounds, particularly polyphenols. Supports hydration. Apple juice is 88% water and tastes good.</p>
-                                        <button className='banner-btn'>Juicy Apple <FontAwesomeIcon style={{ marginLeft: '10px' }} icon={faArrowRight} />  </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
+                
                 <SwiperSlide>
                     <div className="item-2-container">
                         <div className="container">
@@ -47,9 +32,25 @@ const Banner = () => {
                                         <div className="product-banner2-title">
                                             <p className='feaured-title'>Feature Product</p>
                                             <h2>Most  <span className='highlight'> Nutrient </span> lively  Vegetables </h2>
-                                            <p>Check the characteristic signs of freshness such as bright, lively color in the vegetable and look to see if the vegetable is crisp and free of soft spots.</p>
+                                            <p className='pl-3'>Check the characteristic signs of freshness such as bright, lively color in the vegetable and look to see if the vegetable is crisp and free of soft spots.</p>
                                             <button className='banner-btn'>Vegetables <FontAwesomeIcon style={{ marginLeft: '10px' }} icon={faArrowRight} /></button>
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="item-container">
+                        <div className="container">
+                            <div className='row'>
+                                <div className="col-lg-6 col-md-6 ">
+                                    <div className='product-banner-title '>
+                                        <p className='feaured-title'>Feature Product</p>
+                                        <h2>May <span className='highlight'>Protect</span> your brain as you age</h2>
+                                        <p>Contains beneficial plant compounds. Apples are rich in plant compounds, particularly polyphenols. Supports hydration. Apple juice is 88% water and tastes good.</p>
+                                        <button className='banner-btn'>Juicy Apple <FontAwesomeIcon style={{ marginLeft: '10px' }} icon={faArrowRight} />  </button>
                                     </div>
                                 </div>
                             </div>
