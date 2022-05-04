22703b85976a16fe97680db5f2c16cc310166da8
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-05-04 18:57:29 +0600

    update quantity number

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -36,13 +36,43 @@ const Inventory = () => {
                 const id = inventoryId;
                 const url = `http://localhost:5000/products/${id}`;
                 fetch(url)
-                .then(res => res.json())
-                .then(data => setSinleProduct(data))
+                    .then(res => res.json())
+                    .then(data => setSinleProduct(data))
 
             })
 
 
     }
+
+
+    // handleItemQuantityNumber 
+    const handleItemQuantityNumber = e => {
+        e.preventDefault()
+        const userInput = parseInt(e.target.storeQuantity.value);
+        const inputValue = singleProduct.quantity + userInput;
+        console.log(inputValue)
+        const reStored = { inputValue };
+        const url = `http://localhost:5000/product/${inventoryId}`;
+        fetch(url, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(reStored)
+        })
+            .then(res => res.json())
+            .then(data => {
+                const id = inventoryId;
+                const url = `http://localhost:5000/products/${id}`;
+                fetch(url)
+                    .then(res => res.json())
+                    .then(data => setSinleProduct(data))
+            })
+
+        e.target.reset();
+    }
+
+
 
     return (
         <div className='mt-5 py-5'>
@@ -50,18 +80,18 @@ const Inventory = () => {
                 usePageTitle('Stock Item')
             }
             <div className="container ">
-                <div className="row singleWrapper">
-                    <div className="col-lg-5">
+                <div className="row d-flex align-items-center singleWrapper">
+                    <div className="col-lg-5 col-md-8 d-block m-auto">
                         <article className="singleProduct">
                             <div className='singleProduct-frame'>
                                 <img src={img} alt="" />
                             </div>
                             <div className="product-info">
+                                <h6>Id: <span className='id-style'> {_id}</span></h6>
                                 <h4>{name}</h4>
                                 <h5>Price: ${price}</h5>
                                 <h6>Quantity: <span className='text-danger quantity'>{quantity}</span> </h6>
                                 <h6>Sold: ${sold}</h6>
-                                <h6>Id: {_id}</h6>
                                 <h6>Suplier: By <span className='text-info'>{supliername}</span></h6>
                                 <p>{description}</p>
                             </div>
@@ -70,8 +100,15 @@ const Inventory = () => {
                             </div>
                         </article>
                     </div>
-                    <div className="col-lg-7">
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto praesentium suscipit unde! Commodi facere vero doloribus et, hic quod fugiat!</p>
+                    <div className='col-lg-1 restored'></div>
+                    <div className="col-lg-6 col-md-6 ">
+                        <div className='store-item-box'>
+                            <h3>Restored Item</h3>
+                            <form onSubmit={handleItemQuantityNumber}>
+                                <input type="number" name='storeQuantity' placeholder='Enter quantity number' required /> <br />
+                                <button>ReStored Product</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
