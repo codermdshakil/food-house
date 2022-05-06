d771593be7ed937291aa2e67a660f96211f92fea
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-05-07 01:58:49 +0600

    my items added and delete

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import useStoredProducts from '../../hooks/useStoredProducts';
+import React, { useEffect, useState } from 'react';
+// import useStoredProducts from '../../hooks/useStoredProducts';
 import MyItem from '../MyItem/MyItem';
-import usePageTitle  from '../../hooks/usePageTitle';
+import usePageTitle from '../../hooks/usePageTitle';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const MyItems = () => {
 
     const [user] = useAuthState(auth);
-    console.log(user)
 
-    
-    const [products, setProducts] = useStoredProducts()
-    const myItems = products.filter(item => item.email);
+    const [allMyItems, setAllMyItems] = useState([]);
 
-    
+    useEffect(() => {
+        const email = user.email;
+        const url = `https://calm-waters-38040.herokuapp.com/myitems?email=${email}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllMyItems(data))
+    }, [user])
+
+
+
+
     const handleMyitemDelete = id => {
         const confirmationMessage = window.confirm('Are you sure to delete this item?');
         if (confirmationMessage) {
-            const url = `https://calm-waters-38040.herokuapp.com/items/${id}`;
+            const url = `https://calm-waters-38040.herokuapp.com/myitem/${id}`;
             fetch(url, {
                 method: "DELETE",
             })
                 .then(res => res.json())
                 .then(data => {
-                    const newProducts = products.filter(item => item._id !== id);
-                    setProducts(newProducts)
+                    const newProducts = allMyItems.filter(item => item._id !== id);
+                    setAllMyItems(newProducts)
                 })
         }
     }
@@ -37,10 +44,10 @@ const MyItems = () => {
                 usePageTitle('My Items')
             }
             <div className="container">
-                <h3 className='text-center py-3 mb-3'>My Item's <span style={{ color: 'red' }}>({myItems.length})</span>  </h3>
+                <h3 className='text-center py-3 mb-3'>My Item's <span style={{ color: 'red' }}>({allMyItems.length})</span>  </h3>
                 <div className="row">
                     {
-                        myItems.map(item => <MyItem key={item._id} item={item} handleMyitemDelete={handleMyitemDelete}></MyItem>)
+                        allMyItems.map(item => <MyItem key={item._id} item={item} handleMyitemDelete={handleMyitemDelete}> </MyItem>)
                     }
                 </div>
             </div>
